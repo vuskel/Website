62fe991eaa848f6103d534cadc9a756f1c9f8235
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-11 22:05:09 +0530

    Update index.js

--- common/index.js
+++ common/index.js
@@ -1,3 +1,17 @@
+AOS.init();
+
+// Scroll to top button
+const btnScrollToTop = document.querySelector("#header");
+
+btnScrollToTop.addEventListener("click", function(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
+// Common header and footer
 $(function(){
     $("#header").load("/common/header.html");
     $("#footer").load("/common/footer.html");
