61d4e69e4a71a1c9462a18e6154792c5fba21227
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-12 09:53:07 +0530

    Update Scroll to top (#178)

--- common/index.js
+++ common/index.js
@@ -1,17 +1,5 @@
 AOS.init();
 
-// Scroll to top button
-const btnScrollToTop = document.querySelector("#btnScrollToTop");
-
-btnScrollToTop.addEventListener("click", function(){
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-});
-
-// Common header and footer
 $(function(){
     $("#header").load("/common/header.html");
     $("#footer").load("/common/footer.html");
